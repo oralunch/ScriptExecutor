a319e4615cf3707f99f994ae332ae078299c53cc
lunchwu <61185911@qq.com>
2013-06-22 11:41:38 +0800

    first commit

---
+++ ScriptExecutor/ScriptExecutor/model/DatabaseHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace ScriptExecutor
+{
+    class DatabaseHandler
+    {
+        public OracleConnection GetConnection()
+        {
+            string connStr = @"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                             @"(HOST=127.0.0.1)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl)));" +
+                              "Persist Security Info=True;USER ID=scott;password=tiger;" +
+                              "Min Pool Size=5;Max Pool Size=30;Connection Lifetime=120;";
+            OracleConnection conn = new OracleConnection(connStr);
+
+            return conn;
+        }
+
+        public OracleDataReader GetResult(string sql, OracleConnection conn)
+        {
+            OracleCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+            OracleDataReader result = comm.ExecuteReader();
+            
+            return result;
+        }
+    }
+}
---
+++ ScriptExecutor/ScriptExecutor/view/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Oracle.DataAccess.Client;
+
+namespace ScriptExecutor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DatabaseHandler dbh = new DatabaseHandler();
+            OracleConnection conn = dbh.GetConnection();
+            conn.Open();
+            OracleDataReader rd = dbh.GetResult("select ename from emp", conn);
+            string userList = null;
+            while (rd.Read())
+            {
+                userList = userList + " " + rd.GetString(0);
+            }
+
+            MessageBox.Show(userList);
+            rd.Close();            
+            conn.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            folderDialog.ShowDialog();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txbScript.Text = Environment.CurrentDirectory;
+            txbLog.Text = Environment.CurrentDirectory;
+        }
+
+        private void btnScript_Click(object sender, EventArgs e)
+        {
+            folderDialog.ShowDialog();
+            txbScript.Text = folderDialog.SelectedPath;
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            folderDialog.ShowDialog();
+            txbLog.Text = folderDialog.SelectedPath;
+        }
+    }
+}
