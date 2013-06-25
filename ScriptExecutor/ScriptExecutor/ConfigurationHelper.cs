5882ae3f0c65890ad6a939e808f38605c36f1987
lunch <61185911@qq.com>
2013-06-25 14:20:00 +0800

    Add ConfigurationHelper
    GetConfig method
    UpdateConfig method

---
+++ ScriptExecutor/ScriptExecutor/ConfigurationHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace ScriptExecutor
+{
+    class ConfigurationHelper
+    {
+        public string GetAppConfig(string configName)
+        {
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                if (key == configName)
+                {
+                    return ConfigurationManager.AppSettings[configName];
+                }
+            }
+
+            return null;
+        }
+
+        public void UpdateAppConfig(string configName, string configValue)
+        {
+            bool isModified = false;
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                if (key == configName)
+                {
+                    isModified = true;
+                }
+            }
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (isModified)
+            { 
+                config.AppSettings.Settings.Remove(configName); 
+            }
+            config.AppSettings.Settings.Add(configName, configValue);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
