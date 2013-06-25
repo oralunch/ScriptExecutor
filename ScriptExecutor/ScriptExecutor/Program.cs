d92d548694d820876978d7554ce46be2f751edc9
lunch <61185911@qq.com>
2013-06-25 14:23:59 +0800

    remove LoginForm

--- ScriptExecutor/ScriptExecutor/Program.cs
+++ ScriptExecutor/ScriptExecutor/Program.cs
@@ -16,7 +16,7 @@ namespace ScriptExecutor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmMain());
         }
     }
 }
